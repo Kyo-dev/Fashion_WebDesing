351207795db93e6a0140e845962d33acf811e308
haku <chris_db@icloud.com>
2018-05-02 20:34:53 -0600

    initial configure complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,27 @@
-const gulp = require(`gulp`)
-const sass = require(`gulp-sass`)
-const browserSync = require('browser-sync').create();
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
 // sass
 gulp.task('sass', function () {
-    return gulp.src('./scss/**/*.scss')
+    gulp.src('./scss/styless.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
 });
 
 //  Browsersync
 gulp.task('default', function() {
 
     browserSync.init({
-        server: "./app"
+        server: "./"
     });
 
-    // gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("./scss/styless.scss", ['sass']).on('change', browserSync.reload);
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
