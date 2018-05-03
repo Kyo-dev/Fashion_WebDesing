01e699e3a0198e44deb320ebb0926fc842e7405e
haku <chris_db@icloud.com>
2018-05-02 19:40:36 -0600

    Browsersync configured

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 const gulp = require(`gulp`)
-var sass = require(`gulp-sass`)
+const sass = require(`gulp-sass`)
+const browserSync = require('browser-sync').create();
 
+// sass
 gulp.task('sass', function () {
     return gulp.src('./scss/**/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./css'));
+});
+
+//  Browsersync
+gulp.task('default', function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    // gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
